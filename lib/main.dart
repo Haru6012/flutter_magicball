81ffac0f9cf973f3f3207c517418b48833bc2811
松坂波季 <matuharu@matsuzakahakinoMacBook-Pro.local>
2021-10-22 21:07:33 +0900

    add

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+      MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.blue.shade900,
+            title: Text("Ask Me Anything"),
+          ),
+          body: MainPage(),
+          backgroundColor: Colors.blue,
+        ),
+      ),
+    );
+
+class MainPage extends StatefulWidget {
+  const MainPage({Key key}) : super(key: key);
+
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  var ballNumber = 3;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: TextButton(
+        onPressed: () {
+          setState(() {
+            ballNumber = Random().nextInt(5) + 1;
+          });
+        },
+        child: Image.asset("images/ball$ballNumber.png"),
+      ),
+    );
+  }
+}
